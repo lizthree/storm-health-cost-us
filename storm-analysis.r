e21d252f38520566fa598a271bb4d55c9f9559a4
liz <lizanderson.ca@gmail.com>
2019-06-02 14:36:12 -0700

    more plots to play with

--- storm-analysis.r
+++ storm-analysis.r
@@ -30,7 +30,7 @@ health <- separate(health,date,into=c("year","month","day"),sep="-", drop=TRUE)
 # > dim(health)
 # [1] 902297      5
 
-# unique(health$event[grepl("warm", health$event)])
+# unique(health$event[grepl("hail", health$event)])
 # combine like-data, found that certain data should be considered together
 # hurricane's don't compare to other high-fatalities events
 # different variations of heat at the top
@@ -45,30 +45,60 @@ health$event[grepl("flood|flooding",health$event)] <- "flooding/flash flooding"
 health$event[grepl("current",health$event)] <- "rip current"
 health$event[grepl("tstm|thunderstorm wind",health$event)] <- "thunderstorm wind"
 health$event[grepl("fire",health$event)] <- "fire/wildfire"
-health$event <- factor(health$event)
+health$event[grepl("hail",health$event)] <- "hail"
+#health$event <- factor(health$event)
 
 health_fatalities <- health %>%
-  select(event, year, fatalities, injuries) %>%
-  filter(year > "1989" & fatalities > 0) %>%
+  select(event, year, fatalities) %>%
+  filter(year > "1992" & fatalities > 0) %>%
   group_by(year,event) %>%
   summarise(fatalities = sum(fatalities)) %>%
-  arrange(desc(fatalities))
+  arrange(year, event, fatalities) 
 
-state_fatalities <- health %>%
-  select(event, state, fatalities) %>%
-  filter(fatalities > 0) %>%
-  group_by(state,event) %>%
-  summarise(fatalities = sum(fatalities)) %>%
-  arrange(desc(fatalities))
+top_3_fatalities_per_year <- health_fatalities %>% 
+  top_n(n=3, wt=fatalities) %>%
+  arrange(year, event, fatalities)
 
-top_3_per_year <- health_fatalities %>% 
-                  group_by(year) %>% 
-                  top_n(n=3, wt=fatalities) %>%
-                  arrange(year, event, fatalities)
+other_fatalities <- setdiff(health_fatalities, top_3_fatalities_per_year)
+other_fatalities$event = "other"
+other_fatalities_per_year <- other_fatalities %>% 
+  group_by(year, event) %>%
+  summarise(fatalities = sum(fatalities))
+
+# combine all data
+all_fatalities_year_event <- rbind(top_3_fatalities_per_year, other_fatalities_per_year)
+all_fatalities <- all_fatalities_year_event %>%
+  group_by(year, event) %>%
+  summarise(fatalities) %>%
+  mutate(freq = round(100 * fatalities/sum(fatalities), 0)) # paste0(round(100 * fatalities/sum(fatalities), 0), "%")
 
 
-g <- ggplot(top_3_per_year, aes(x = year, y = fatalities, fill = event))
+g <- ggplot(all_fatalities, aes(x = year, y = fatalities, fill = event))
 g + geom_bar(stat="identity", width = 0.5) + 
+  geom_text(data=subset(all_fatalities,event!="other"), aes(label=ifelse(fatalities == pmax(fatalities), fatalities, "")), size=3) + # data=subset(all_fatalities,event!="other"),
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  labs(title="Top 3", 
+       subtitle="Manufacturer across Vehicle Classes")
+
+max_per_year <- subset(all_fatalities, event!="other") %>% group_by(year) %>% filter(fatalities==max(fatalities))
+
+g <- ggplot(top_3_fatalities_per_year, aes(x = year, y = fatalities, fill = event))
+g + geom_point(stat="identity", size=7, shape=21, color="black") + 
+  geom_text(data=max_per_year, aes(label=pmax(fatalities)), size=2) + # data=subset(all_fatalities,event!="other"),
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  labs(title="Top 3", 
+       subtitle="Manufacturer across Vehicle Classes")
+
+g <- ggplot(top_3_fatalities_per_year, aes(x = year, y = fatalities, fill = event))
+g + geom_bar(stat="identity", width=0.5) + 
+  geom_text(data=max_per_year, aes(label=pmax(fatalities)), size=3) + # data=subset(all_fatalities,event!="other"),
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  labs(title="Top 3", 
+       subtitle="Manufacturer across Vehicle Classes")
+
+
+g <- ggplot(health_fatalities_max, aes(x = year, y = fatalities, fill = event))
+g + geom_bar(stat="identity", width = 0.5, position = "dodge") + 
   theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
   labs(title="Top 3", 
        subtitle="Manufacturer across Vehicle Classes")
